bd4804bfa0c8d5aecf9fd1ce5455ef44b86fc310
Filipe Castro <filipecsant@gmail.com>
2023-12-15 20:47:39 -0300

    iniciando ajustes

---
+++ DescomplicaEventos.API/Controllers/UsuarioController.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DescomplicaEventos.API.Controllers.Shared;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DescomplicaEventos.API.Controllers
+{
+    [Route("v1/[controller]")]
+    public class UsuarioController : ApiControllerBase
+    {
+
+    }
+}
