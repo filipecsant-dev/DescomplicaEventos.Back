712b90eb60ce4214a33c487d2027b97e3e454a82
Filipe Castro <filipecsant@gmail.com>
2023-12-15 20:45:44 -0300

    Revert "iniciando controller usuario"
    This reverts commit 9b63c0150fa6f6d94f3035cf4726cb9c200e470c.

---
+++ DescomplicaEventos.API/Controllers/WeatherForecastController.cs
@@ -0,0 +1,35 @@
+using DescomplicaEventos.API.Controllers.Shared;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DescomplicaEventos.API.Controllers;
+
+[Route("api/[controller]")]
+public class WeatherForecastController : ApiControllerBase
+{
+    private static readonly string[] Summaries = new[]
+    {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+    private readonly ILogger<WeatherForecastController> _logger;
+
+    public WeatherForecastController(ILogger<WeatherForecastController> logger)
+    {
+        _logger = logger;
+    }
+
+    [HttpGet(Name = "GetWeatherForecast")]
+    public IActionResult Get()
+    {
+        throw new Exception("teste");
+        var teste = Enumerable.Range(1, 5).Select(index => new WeatherForecast
+        {
+            Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+            TemperatureC = Random.Shared.Next(-20, 55),
+            Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+        })
+        .ToArray();
+
+        return ResponseOk(teste);
+    }
+}
